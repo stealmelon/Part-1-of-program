91fdea9a8c24e005121058d86af3373b408bad28
枫小冉 <18735267646@163.com>
2019-06-19 18:40:23 +0800

    Initial Commit

---
+++ pro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pro
+//
+//  Created by s20181106115 on 2019/6/19.
+//  Copyright © 2019 s20181106115. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
